d326731524cc3fbde346d9ccf13af00f874b9bf8
Erwin Flores <ramiromarca6@gmail.com>
2018-12-05 08:14:50 -0400

    Actividad Login realizado

---
+++ app/src/main/java/com/myfoodapp/seminarioproyect/myfoodapp/Login.java
@@ -0,0 +1,81 @@
+package com.myfoodapp.seminarioproyect.myfoodapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.myfoodapp.seminarioproyect.myfoodapp.utils.Data;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+//import static android.content.DialogInterface.*;
+
+public class Login extends AppCompatActivity {
+    private Button login;
+    private Button registerNewClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login=(Button) findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginClient();
+            }
+        });
+        registerNewClient=(Button) findViewById(R.id.btn_registerNewClient);
+        registerNewClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void loginClient(){
+        TextView email=findViewById(R.id.email);
+        TextView password= findViewById(R.id.password);
+        AsyncHttpClient client= new AsyncHttpClient();
+        RequestParams params=new RequestParams();
+        params.add("email",email.getText().toString());
+        params.add("password",password.getText().toString());
+
+        client.post(Data.HOST+Data.LOGIN,params,new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                AlertDialog alertDialog = new AlertDialog.Builder(Login.this).create();
+                try{
+                    String token="";response.getString("token");
+                    alertDialog.setTitle("RESPONSE SERVER");
+                    alertDialog.setMessage("Sesion Iniciada");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL,"OK",new DialogInterface.OnClickListener(){
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+                    alertDialog.show();
+                    Data.TOKEN=token;
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+        email.setText(Data.TOKEN);
+    }
+}
---
+++ app/src/main/java/com/myfoodapp/seminarioproyect/myfoodapp/utils/Data.java
@@ -0,0 +1,7 @@
+package com.myfoodapp.seminarioproyect.myfoodapp.utils;
+
+public class Data {
+    public static String TOKEN=null;
+    public static String HOST="http://192.168.100.8:7777/api";
+    public static String LOGIN="/clients/login";
+}
