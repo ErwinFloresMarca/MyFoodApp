7b899be8b6b96502973762fa64133385226ef592
CelindaHuanaco <huanacocelinda@gmail.com>
2018-12-07 18:15:19 -0400

    layout de menu

---
+++ app/src/main/java/com/myfoodapp/seminarioproyect/myfoodapp/RegisterMenu.java
@@ -0,0 +1,13 @@
+package com.myfoodapp.seminarioproyect.myfoodapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_menu);
+    }
+}
